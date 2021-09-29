ca0bebe1fe74e1a31d477ba3deb3b27d99493b90
bernandez <alouzehm@gmail.com>
2021-09-29 11:42:24 +0100

    Created password generator

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script that takes  a users details and automatically generates a password.
+#different functions can be created or modified
+
+
+def password_gen():
+    return password_gen()
+    
+
+
+# Press the green button in the gutter to run the script.
+
+
+
